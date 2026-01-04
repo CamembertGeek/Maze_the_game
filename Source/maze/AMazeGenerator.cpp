9a735939b10f6aa093e9aec798468ab6ca1ab74a
Louis Bersegol <bersegollouis@gmail.com>
2026-01-04 10:04:53 +0100

    Starting to code the maze generation algorithm.

--- Source/maze/AMazeGenerator.cpp
+++ Source/maze/AMazeGenerator.cpp
@@ -3,6 +3,8 @@
 
 #include "AMazeGenerator.h"
 #include <vector>
+#include <cstdint>
+#include <utility> // std::pair
 
 // Sets default values
 AAMazeGenerator::AAMazeGenerator()
@@ -36,13 +38,104 @@ private:
 	int width;
 
 	// Generate a maze grid with Backtracking.
-	void BacktrackingMazeGenerator(int height, int width) {
+	std::vector<uint8_t> BacktrackingMazeGenerator(int height, int width)
+	{
+		// Initialisation of the maze grid.
+		std::vector<uint8_t> Grid(height * width, 1); // 1 = wall
 
-		// Initialisation of the maze grid
-		std::vector<uint8_t> Grid(height * width, 1);  // 1 = wall
+		// Initialisation of the starting point (cell coordinates).
+		std::pair<int, int> StartingPoint = { 1, 1 };
 
+		// Convert (1,1) to 1D index and open it
+		int startIndex = Indexation(StartingPoint.first, StartingPoint.second, width);
+		Grid[startIndex] = 0; // 0 = path
+
+		// Stocking of all the visited point
+		std::vector<int> stack;
+		stack.push_back(startIndex);
+
+		while (!stack.empty()){
+			// Initialization of the neighbors list
+			std::vector<std::pair<int, int>> neighbors;
+
+			int current = stack.back(); // Current cell
+
+			int ci = current / width;
+			int cj = current % width;
+
+
+			// We test all direction to see if there is neighbors
+			// UP
+			int UPi = ci - 2;
+			int UPj = cj;
+			
+			if (UPi >= 1 && UPi < height - 1) {
+				if (UPj >= 1 && UPj < width - 1) {
+
+					int idx = UPi * width + UPj;
+
+					if (Grid[idx] == 1) {
+						neighbors.push_back({ UPi, UPj });
+					}
+				}
+			}
+
+			// DOWN
+			int DOWNi = ci + 2;
+			int DOWNj = cj;
+
+			if (DOWNi >= 1 && DOWNi < height - 1) {
+				if (DOWNj >= 1 && DOWNj < width - 1) {
+				
+					int idx = DOWNi * width + DOWNj;
+
+					if (Grid[idx] == 1) {
+						neighbors.push_back({ DOWNi, DOWNj });
+					}
+				}
+			}
+
+			// LEFT
+			int LEFTi = ci;
+			int LEFTj = cj - 2;
+
+			if (LEFTi >= 1 && LEFTi < height - 1) {
+				if (LEFTj >= 1 && LEFTj < width - 1) {
+
+					int idx = LEFTi * width + LEFTj;
+
+					if (Grid[idx] == 1) {
+						neighbors.push_back({ LEFTi, LEFTj });
+					}
+				}
+			}
+
+			// RIGHT
+			int RIGHTi = ci;
+			int RIGHTj = cj + 2;
+
+			if (RIGHTi >= 1 && RIGHTi < height - 1) {
+				if (RIGHTj >= 1 && RIGHTj < width - 1) {
+
+					int idx = RIGHTi * width + RIGHTj;
+
+					if (Grid[idx] == 1) {
+						neighbors.push_back({ RIGHTi, RIGHTj });
+					}
+				}
+			}
 		
+		}
+			 
+
+		return Grid;
 	}
+
+	// Convert (i, j) into 1D index
+	int Indexation(int i, int j, int width)
+		{
+			return i * width + j;
+		}
 
 	// Add Loops inside of the maze.
 	void AddLoops(){
